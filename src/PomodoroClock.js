da26646dfc8faf951915dde138274b379697ce0e
PDOUU <58252790+pdouu@users.noreply.github.com>
2020-08-05 15:03:42 +0300

    clock design

--- src/PomodoroClock.js
+++ src/PomodoroClock.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./PomodoroClock.css";
 
+import startStopAudio from "./sound/startStop.mp3";
+import incrementDecrementAudio from "./sound/incrementDecrement.mp3";
+import timerAudio from "./sound/timerAudio.mp3";
+
 export default function PomodoroClock() {
+  const [timeLeftMinute, setTimeLeftMinute] = useState("25");
+  const [timeLeftSecond, setTimeLeftSeconds] = useState("00");
+  const [startToggle, setStartToggle] = useState(false);
+
+  const [sessionSetting, setSessionSetting] = useState(25);
+  const [breakSetting, setBreakSetting] = useState(5);
+
+  useEffect(() => {
+    console.log("00" - "1");
+  }, []);
+
+  const startStop = () => {
+    !startToggle ? setStartToggle(true) : setStartToggle(false);
+    playSound("button-audio");
+  };
+
+  const resetAll = () => {
+    setTimeLeftMinute("25");
+    setTimeLeftSeconds("00");
+    playSound("button-audio");
+  };
+
+  const incrementDecrement = (whichSetting, operation) => {
+    if (whichSetting === "session") {
+      if (operation === "increment" && sessionSetting < 60) {
+        setSessionSetting(sessionSetting + 1);
+      } else if (operation === "decrement" && sessionSetting > 0) {
+        setSessionSetting(sessionSetting - 1);
+      }
+    } else {
+      if (operation === "increment" && breakSetting < 60) {
+        setBreakSetting(breakSetting + 1);
+      } else if (operation === "decrement" && breakSetting > 0) {
+        setBreakSetting(breakSetting - 1);
+      }
+    }
+
+    playSound("increment-decrement-audio");
+  };
+  const playSound = (soundSrc) => {
+    const soundFile = document.getElementById(soundSrc);
+    soundFile.currentTime = 0;
+    soundFile.play();
+  };
+
   return (
     <div className="clock-container">
       <div className="clock-row">
@@ -10,22 +59,29 @@ export default function PomodoroClock() {
             Session
           </label>
           <div id="time-left">
-            <span className="time-left-minusecs">25</span>
+            <span className="time-left-minusecs">{timeLeftMinute}</span>
             <span id="minusecs-between">:</span>
-            <span className="time-left-minusecs">00</span>
+            <span className="time-left-minusecs">{timeLeftSecond}</span>
           </div>
+          <audio id="timer-audio" src={timerAudio}></audio>
         </div>
       </div>
       <div className="clock-row-start-stop">
         <i
           id="start_stop"
           className={
-            true
+            !startToggle
               ? "clock-icon-start-stop fas fa-play-circle"
               : "clock-icon-start-stop fas fa-pause-circle"
           }
+          onClick={() => startStop()}
         ></i>
-        <i id="reset" className="clock-icon-start-stop fas fa-undo"></i>
+        <i
+          id="reset"
+          className="clock-icon-start-stop fas fa-undo"
+          onClick={() => resetAll()}
+        ></i>
+        <audio id="button-audio" src={startStopAudio}></audio>
       </div>
       <div className="clock-row-break-session">
         <div id="session-container" className="break-session-settings">
@@ -40,11 +96,13 @@ export default function PomodoroClock() {
             <i
               id="session-increment"
               className="clock-icon-plus-minus fas fa-plus-square"
+              onClick={() => incrementDecrement("session", "increment")}
             ></i>
-            <span id="session-length">25</span>
+            <span id="session-length">{sessionSetting}</span>
             <i
               id="session-decrement"
               className="clock-icon-plus-minus fas fa-minus-square"
+              onClick={() => incrementDecrement("session", "decrement")}
             ></i>
           </div>
         </div>
@@ -60,12 +118,18 @@ export default function PomodoroClock() {
             <i
               id="break-increment"
               className="clock-icon-plus-minus fas fa-plus-square"
+              onClick={() => incrementDecrement("break", "increment")}
             ></i>
-            <span id="break-length">5</span>
+            <span id="break-length">{breakSetting}</span>
             <i
               id="break-decrement"
               className="clock-icon-plus-minus fas fa-minus-square"
+              onClick={() => incrementDecrement("break", "decrement")}
             ></i>
+            <audio
+              id="increment-decrement-audio"
+              src={incrementDecrementAudio}
+            ></audio>
           </div>
         </div>
       </div>
