f28c98c5a18501a035c794b65b2f8a77e645d62a
PDOUU <58252790+pdouu@users.noreply.github.com>
2020-08-14 21:28:38 +0300

    Updates

---
+++ precache-manifest.f1402cfbb958c9dff98d283a8f92256b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb9cfdd88b587000baf0ac28d90f00fa",
+    "url": "/fcc-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "898c9d1c5efefdcd01b1",
+    "url": "/fcc-pomodoro-clock/static/css/main.4b29c5b1.chunk.css"
+  },
+  {
+    "revision": "2d6f14c355eb3a7dd4e8",
+    "url": "/fcc-pomodoro-clock/static/js/2.11df3ca3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fcc-pomodoro-clock/static/js/2.11df3ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "898c9d1c5efefdcd01b1",
+    "url": "/fcc-pomodoro-clock/static/js/main.c602a92a.chunk.js"
+  },
+  {
+    "revision": "d504f752ccbadbeb51fb",
+    "url": "/fcc-pomodoro-clock/static/js/runtime-main.64532136.js"
+  },
+  {
+    "revision": "f332e318890f1c16b660ebe9c2e6a592",
+    "url": "/fcc-pomodoro-clock/static/media/incrementDecrement.f332e318.mp3"
+  },
+  {
+    "revision": "2fcb86887e718d77fe41309fe15a8eb9",
+    "url": "/fcc-pomodoro-clock/static/media/startStop.2fcb8688.mp3"
+  },
+  {
+    "revision": "ba877f66d289e626a7da55a2050cdcb9",
+    "url": "/fcc-pomodoro-clock/static/media/timerAudio.ba877f66.mp3"
+  }
+]);
