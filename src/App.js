e94839ff4cedf775b09ef085d5500a1e4667d8ac
PDOUU <58252790+pdouu@users.noreply.github.com>
2020-12-20 18:17:32 +0300

    changed links

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ function App() {
       <div id="pomodoro-h1">Pomodoro Clock</div>
       <PomodoroClock />
       <p style={{ fontSize: "20px" }}>
-        Created by{" "}
         <a
+          className="github-link"
           rel="noopener noreferrer"
           target="_blank"
-          href="https://github.com/pdouu"
+          href="https://github.com/yusufcmlt/fcc-pomodoro-clock"
         >
-          pdouu
+          {"  "}
         </a>
       </p>
     </div>
